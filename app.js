57fc08bb2d47769f3dc2a48fb8a0cebe98309736
AbdallahAlyamni <abdallahalyamni@gmail.com>
2023-03-14 14:04:52 +0300

    Added test case.
    Used quran cloud api to get ayah audio url

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ var port = process.env.PORT || 3000,
     audioconcat = require('audioconcat')
     download = require('download');
     argv = require('minimist')(process.argv.slice(2));
+    path = require('node:path');
+const axios = require('axios');
 
 
-console.log(argv["_"][0]);
+// console.log(argv["_"][0]);
 
-// var log = function (entry) {
-//     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-// };
+var log = function (entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
 
 let base_url = 'https://cdn.islamic.network/quran/audio/128/ar.alafasy/';
 const filePath = `${__dirname}/files`;
@@ -20,70 +22,65 @@ let files = [argv["_"][0], argv["_"][1]];
 if (argv["_"].length == 2) {
     downloadMerge(files, filePath)
 } else {
-
-    console.log("wrong arguments!: you must enter 2 numric values");
-    
+    console.error("wrong arguments!: you must enter 2 numric values");
 }
-// downloadMerge(files, filePath)
+
+
+
 
 async function downlodfiles(file, filePath) {
-    await Promise.all(file.map((file) => download(base_url + file + '.mp3', filePath)))
+    await Promise.all(file.map((file) => download(file, filePath)))
 };
 
-function downloadMerge(files, filePath) {
-    downlodfiles(files, filePath).then(() => {
-        merge(files);
+async function downloadMerge(files, filePath) {
+    let ayaht = await getAyahtURLs(files)
+    downlodfiles(ayaht, filePath).then(() => {
+        merge(ayaht);
     });
+    return 'success';
 }
 
-function merge(files) {
 
-    let output_name = files[0] + "_" + files[files.length - 1] + ".mp3";
+async function getAyahURL(ayah_reference) {
+    try {
+        const response = await axios.get(`https://api.alquran.cloud/v1/ayah/${ayah_reference}/ar.alafasy`);
+        // console.log(response.data);
+        return response.data.data.audio;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+}
 
-    let files_paths = files.map(file => "files/" + file + ".mp3");
+async function getAyahtURLs(ayaht_references) {
+
+    let res = await Promise.all(ayaht_references.map(async (ayah_reference) => await getAyahURL(ayah_reference)));
+    console.log(res);
+    return res;
+    
+    // return ayaht;
+}
+
+ function merge(files) {
+
+    let output_name = path.posix.basename(files[0], '.mp3') + "_" + path.posix.basename(files[files.length - 1], '.mp3') + ".mp3";
+
+    let files_paths = files.map(file => "files/" + path.posix.basename(file));
 
     audioconcat(files_paths)
         .concat(output_name)
-        .on('start', function (command) {
-            console.log('ffmpeg process started:', command)
-        })
         .on('error', function (err, stdout, stderr) {
             console.error('Error:', err)
             console.error('ffmpeg stderr:', stderr)
         })
         .on('end', function (output) {
-            console.error('Audio created in:', output)
-        })
+            console.log('Audio created in:', output)
+        });
+    
+    // let res = await Promise.all(ayaht_references.map(async (ayah_reference) => await getAyahURL(ayah_reference)));
 
 }
 
-// var server = http.createServer(function (req, res) {
-//     if (req.method === 'POST') {
-//         var body = '';
-
-//         req.on('data', function(chunk) {
-//             body += chunk;
-//         });
-
-//         req.on('end', function() {
-//             if (req.url === '/') {
-//                 log('Received message: ' + body);
-//             } else if (req.url = '/scheduled') {
-//                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-//             }
-
-//             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-//             res.end();
-//         });
-//     } else {
-//         res.writeHead(200);
-//         res.write(html);
-//         res.end();
-//     }
-// });
-
-// Listen on port 3000, IP defaults to 127.0.0.1
-// server.listen(port);
-
-// Put a friendly message on the terminal
-// console.log('Server running at http://127.0.0.1:' + port + '/');
+exports.getAyahURL = getAyahURL;
+exports.getAyahtURLs = getAyahtURLs;
+exports.downloadMerge = downloadMerge;
